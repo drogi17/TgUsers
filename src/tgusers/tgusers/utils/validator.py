cf1be8d0e9a8c43ab4f95d4d749b5f58818cc6f7
drogi17 <38162697+drogi17@users.noreply.github.com>
2020-10-06 17:26:09 +0300

    Delete validator.py

--- src/tgusers/tgusers/utils/validator.py
+++
@@ -1,6 +0,0 @@
-def valid_check(string: str):
-    valid_chars = "abcdefghijklmnopqrstuvwxyz1234567890_-"
-    for char in string:
-        if char not in valid_chars:
-            return False
-    return True
